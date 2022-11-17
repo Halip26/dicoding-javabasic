0700f89fb9a91d0128c8de94c4a90b1f81ce6e3a
Halip26 <halipuddin.angko@gmail.com>
2022-11-17 17:38:15 +0800

    prettier

--- src/com/dicoding/operator/OperatorAssignment.java
+++ src/com/dicoding/operator/OperatorAssignment.java
@@ -1,11 +1,11 @@
 package com.dicoding.operator;
 
-public class OperatorAssignment {
-  public static void main(String[] args) {
-    int value = 2;
-    int anotherValue = 19;
-    System.out.println("Data pada value adalah ->> " + value);
-    System.out.println("Data pada anotherValue adalah ->> " + anotherValue);
+  public class OperatorAssignment {
+    public static void main(String[] args) {
+      int value = 2;
+      int anotherValue = 19;
+      System.out.println("Data pada value adalah ->> " + value);
+      System.out.println("Data pada anotherValue adalah ->> " + anotherValue);
 
-}
+  }
 }
