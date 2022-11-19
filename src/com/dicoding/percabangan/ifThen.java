c688850c5e46441ccd43899dfbc01a38098dea2f
Halip26 <halipuddin.angko@gmail.com>
2022-11-19 22:45:29 +0800

    add package & class

---
+++ src/com/dicoding/percabangan/ifThen.java
@@ -0,0 +1,10 @@
+package com.dicoding.percabangan;
+ 
+public class ifThen {
+    public static void main(String[] args) {
+        boolean isOn = true;
+        if (isOn) {
+            System.out.println("Menyalakan lampu");
+        }
+    }
+}
