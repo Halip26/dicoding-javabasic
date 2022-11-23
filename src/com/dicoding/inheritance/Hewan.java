7cd98f11c6bc239dc801d302fa4cccdbc6db5d14
Halip26 <halipuddin.angko@gmail.com>
2022-11-23 20:00:55 +0800

    Polymorphism inheritance package

--- src/com/dicoding/inheritance/Hewan.java
+++ src/com/dicoding/inheritance/Hewan.java
@@ -2,6 +2,10 @@ package com.dicoding.inheritance;
 
 public class Hewan {
   public Hewan() {
-    System.out.println("construct Hewan");
+      System.out.println("construct Hewan");
+  }
+
+  public void makan() {  // base method
+      System.out.println("Hewan sedang makan..");
   }
 }
