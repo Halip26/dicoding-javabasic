5c6bb1d9ac3845ae4509779ba5f04f1a04c8118f
Halip26 <halipuddin.angko@gmail.com>
2022-11-24 19:24:49 +0800

    update the lines

--- src/com/dicoding/inputoutput/UserBufferedReader.java
+++ src/com/dicoding/inputoutput/UserBufferedReader.java
@@ -3,7 +3,7 @@ package com.dicoding.inputoutput;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
- 
+
 public class UserBufferedReader {
  
     public static void main(String[] args) {
