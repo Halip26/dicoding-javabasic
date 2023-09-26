d5d804d166788da429234d8437f107e8452f120e
Halip26 <halipuddin.angko@gmail.com>
2023-09-26 19:00:50 +0800

    add forefer loop

--- src/herNextplan.java
+++ src/herNextplan.java
@@ -3,13 +3,19 @@ import java.util.Scanner;
 public class herNextplan {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter her next plan: ");
-        String herNextPlan = scanner.nextLine();
         
-        if (herNextPlan.equals("get married")) {
-            System.out.println("I'm going to propose to her");
-        } else {
-            System.out.println("I'll wait for her patiently 😇");
+        boolean isRunning = true;
+        
+        while (isRunning) {
+            System.out.print("Enter her next plan?: ");
+            String herNextPlan = scanner.nextLine();
+            
+            if (herNextPlan.equals("get married")) {
+                System.out.println("I'm going to propose to her");
+                isRunning = false;
+            } else {
+                System.out.println("I'll wait for her patiently!");
+            }
         }
         
         scanner.close();
