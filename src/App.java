da63deda722db23e1eb15db15e4b1d4d341a96bb
Halip26 <halipuddin.angko@gmail.com>
2023-12-24 07:42:58 +0800

    make it original

--- src/App.java
+++ src/App.java
@@ -1,16 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String namaAku = "Halipuddin";
-        int umur = 28;
-
-        boolean suka = true;
-
-        System.out.println("Hello, Nama aku adalah "+ namaAku + " umur aku " + umur);
-
-        if (suka) {
-            System.out.printf("aku jga suka");
-        } else {
-            System.out.printf("aku tidak suka");
-        }
-    }   
+        System.out.println("Hello, World!");
+    }
 }
